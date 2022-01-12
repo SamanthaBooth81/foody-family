2b71455f68419e47e23aad4aed8283ca0ba3ed60
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-01-12 17:05:34 +0000

    create base template and index.html file and views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
+"""Views for the different pages to be rendered"""
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    """View for the list of recipes posted"""
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 10
