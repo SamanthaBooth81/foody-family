81f792b55e0774885e9192a8b5279a6bee533e37
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-01-13 16:42:27 +0000

    amend paginate_by to 12

--- recipes/views.py
+++ recipes/views.py
@@ -9,4 +9,4 @@ class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 10
+    paginate_by = 12
