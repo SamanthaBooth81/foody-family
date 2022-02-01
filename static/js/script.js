2592c572358f85930d53ef3703875ddc7ba7d032
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-02-01 15:16:12 +0000

    remove bootstrap toast code

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,9 @@
-var toastTrigger = document.getElementById('liveToastBtn')
-var toastLiveExample = document.getElementById('liveToast')
-if (toastTrigger) {
-  toastTrigger.addEventListener('click', function () {
-    var toast = new bootstrap.Toast(toastLiveExample)
+// var toastTrigger = document.getElementById('liveToastBtn')
+// var toastLiveExample = document.getElementById('liveToast')
+// if (toastTrigger) {
+//   toastTrigger.addEventListener('click', function () {
+//     var toast = new bootstrap.Toast(toastLiveExample)
 
-    toast.show()
-  })
-}
+//     toast.show()
+//   })
+// }
