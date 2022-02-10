c28d2d1795cd16da50dfcfc715a5d6c910360808
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-02-10 09:47:51 +0000

    Add delete instruction functionality

--- static/js/script.js
+++ static/js/script.js
@@ -6,13 +6,18 @@
 //     let alert = new bootstrap.Alert(messages);
 //     alert.close()}, 3000);
 
+function deleteInstruction(target){
+    divId = target.getAttribute('data-instruction-id')
+    document.getElementById(divId).remove();
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
     let instructionsCounter = 0;
 
 
     function add_instruction(event){
-        console.log('add');
+        // console.log('add');
         instructionId = 'instruction_' + instructionsCounter;
         new_instruction = '<div style="margin-top: 5px; margin-left: 15px;" id="' + instructionId + '"> <input type="text" name="instructions[]"> <button type="button" onClick="deleteInstruction(this)" class="delete_instruction" data-instruction-id="'+ instructionId +'">Delete</button></div>';
         instructionsCounter = instructionsCounter + 1;
@@ -23,5 +28,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
    
     document.getElementById('add_step').addEventListener('click', add_instruction);
-
 });
