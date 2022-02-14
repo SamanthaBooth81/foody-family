498ef436c806dfe0d98afdc9033e6dc937bbed34
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-02-14 21:13:33 +0000

    breakpoints for login and register forms

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // Below from Code Institutes CodeStar project
 
-// CAUSING AN ERROR IN CONSUL 
-// setTimeout(function() {
-//     let messages = document.getElementById('msg');
-//     let alert = new bootstrap.Alert(messages);
-//     alert.close()}, 3000);
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close()
+}, 3000);
