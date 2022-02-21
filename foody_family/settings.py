64590f5a7e04ca30f88831da510dfae369be38cf
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-02-21 15:33:57 +0000

    remove media from installed apps

--- foody_family/settings.py
+++ foody_family/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'media',
     'django_summernote',
     'django.forms',
     'crispy_forms',
@@ -161,7 +160,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_URL = '/media/'
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+Default primary key field type
+https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
