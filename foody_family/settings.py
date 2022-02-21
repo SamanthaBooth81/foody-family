d944fa9e25c625a10d446019a8758b71ea179ad2
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-02-21 15:36:50 +0000

    comment out code accidentally uncommented out

--- foody_family/settings.py
+++ foody_family/settings.py
@@ -160,7 +160,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-Default primary key field type
-https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
